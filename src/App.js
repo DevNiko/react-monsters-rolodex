bd0523c94d94be150a0e163a4047844547d40b47
DevNiko <nikolinuks@Desktop-Linux>
2020-04-11 17:02:35 +0200

    Prettier and esLint

--- src/App.js
+++ src/App.js
@@ -1,39 +1,37 @@
-import React, { Component} from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react"
+import logo from "./logo.svg"
+import "./App.css"
 
 class App extends Component {
   constructor() {
-    super();
+    super()
 
     this.state = {
       monsters: [
         {
-          id: 'as1w',
-          name: 'Frankenstein'
+          id: "as1w",
+          name: "Frankenstein",
         },
         {
-          id: 'as2w',
-          name: 'Dracula'
+          id: "as2w",
+          name: "Dracula",
         },
         {
-          id: 'as3w',
-          name: 'Zombie'
-        }
-      ]
+          id: "as3w",
+          name: "Zombie",
+        },
+      ],
     }
   }
 
-  render () {
-    return ( 
-        <div className="App">
-          {
-            this.state.monsters.map(monster => (
-              <h1 key={monster.id}>{monster.name}</h1>
-            ))
-          }
-        </div>
-    );
-    }
-};
-export default App;
+  render() {
+    return (
+      <div className="App">
+        {this.state.monsters.map((monster) => (
+          <h1 key={monster.id}>{monster.name}</h1>
+        ))}
+      </div>
+    )
+  }
+}
+export default App
